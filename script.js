41540a35990f3107bab8596d6bfc6dce0ffa1875
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-07-25 17:43:43 +0300

    collecting form input

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function akanName(){
     event.preventDefault();
+    //collect form data
+    var getdate = document.getElementById("dayofbirth").value;
+
+    try {
+
+        if (getdate == "") throw "Empty"
+    }
+
+    catch {
+        document.getElementById("enterDate").innerHTML = "*Date field required"
+        
+   }
 }
